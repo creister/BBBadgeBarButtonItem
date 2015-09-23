5faf805956bf82cad301588c165ba6f379ed2069
Colin Reisterer <creisterer@projectdecibel.com>
2015-09-23 17:38:11 -0400

    Share default scheme

---
+++ Example/BadgeBarButtonFramework/BadgeBarButtonFramework.h
@@ -0,0 +1,19 @@
+//
+//  BadgeBarButtonFramework.h
+//  BadgeBarButtonFramework
+//
+//  Created by creisterer on 9/23/15.
+//  Copyright © 2015 aladenise. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BadgeBarButtonFramework.
+FOUNDATION_EXPORT double BadgeBarButtonFrameworkVersionNumber;
+
+//! Project version string for BadgeBarButtonFramework.
+FOUNDATION_EXPORT const unsigned char BadgeBarButtonFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BadgeBarButtonFramework/PublicHeader.h>
+
+
